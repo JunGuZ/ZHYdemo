2f1c2edff63d8512ce9bc91b65b2f4aeee9617ae
mqy <1876797770@qq.com>
2024-09-25 10:43:18 +0800

    初始化项目

---
+++ src/main/java/com/mqy/ZHYdemo/ZhYdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mqy.ZHYdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhYdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhYdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mqy/ZHYdemo/ZhYdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqy.ZHYdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhYdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
