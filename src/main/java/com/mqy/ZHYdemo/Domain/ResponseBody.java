64bb07e0bcb77276907d0923a4855f8796d6781e
mqy <1876797770@qq.com>
2024-09-26 08:59:44 +0800

    统一化出参

---
+++ src/main/java/com/mqy/ZHYdemo/Domain/ResponseBody.java
@@ -0,0 +1,37 @@
+package com.mqy.ZHYdemo.Domain;
+
+public class ResponseBody {
+    private int code;
+    private String info;
+    private Object data;
+
+    public ResponseBody(int code, String info, Object data) {
+        this.code = code;
+        this.info = info;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
